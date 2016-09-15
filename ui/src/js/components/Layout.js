4f1cf18ddf3af704a234e898f7d1d195f235cdc8
Dynamo Stack <9chakri+ds@gmail.com>
2016-09-15 16:29:16 -0400

    ui

---
+++ ui/src/js/components/Layout.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import Footer from "./Footer";
+import Header from "./Header";
+import axios from "axios";
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';  // in ECMAScript 6
+// or
+//var ReactBsTable = require("react-bootstrap-table");
+//var BootstrapTable = ReactBsTable.BootstrapTable;
+//var TableHeaderColumn = ReactBsTable.TableHeaderColumn;
+
+export default class Layout extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      products:[]
+    };
+    var that = this;
+    setInterval(function(){
+      axios.get("http://localhost:8082/getuserscore")
+        .then((response) => {
+          console.log(response.data); 
+          that.setState({products:response.data});
+        })
+        .catch((err) => {
+          
+        })
+    },3000);
+  }
+
+  
+
+  render() {
+    return (
+      <BootstrapTable data={this.state.products} striped={true} hover={true}>
+        <TableHeaderColumn isKey={true} dataField="name">UserId</TableHeaderColumn>
+        <TableHeaderColumn  dataField="score">Score</TableHeaderColumn>
+      </BootstrapTable>
+    );
+  }
+}
